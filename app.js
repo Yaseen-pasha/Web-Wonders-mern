72a0a66e2a051737fe0a638a01f10298ae0840e2
Yaseen Shaikh <yaseenpadha7296@gmail.com>
2023-02-12 10:58:28 +0530

    make changes to home page

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 // we link the router files to make our route easy 
 app.use(require('./router/auth'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 // if(process.env.NODE_ENV=== "production"){
     app.use(express.static("client/build"));
